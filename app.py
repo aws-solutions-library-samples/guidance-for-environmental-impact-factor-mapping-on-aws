98e0d9ed51d1137be9b4fef1df49429516bb404e
Edmund Chute <echute@amazon.com>
2023-07-27 09:43:29 -0700

    add-stack-description

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from guidance_for_environmental_impact_factor_mapping_on_aws.eifm_stack import E
 app = cdk.App()
 
 # Create single stack within that app
-EifmStack(app, "EifmStack",)
+EifmStack(app, "EifmStack", description="Guidance for Environmental Impact Factor Mapping on AWS (SO9244)")
 
 # Run cdk-nag on the app
 cdk.Aspects.of(app).add(cdk_nag.AwsSolutionsChecks())
